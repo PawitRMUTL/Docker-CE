0fe59032472125467e0aa5231ba7d479487994a9
pawit pitukthong <97580507+PawitRMUTL@users.noreply.github.com>
2024-05-31 22:11:36 +0700

    update code for AWS

--- CEreform-frond-end/env.js
+++ CEreform-frond-end/env.js
@@ -1,6 +1,6 @@
 /** @format */
 // const hostBackend = 'http://0.0.0.0:3000';
-const hostBackend = 'http://192.168.5.88:3000';
+const hostBackend = 'http://18.139.193.101';
 const urlImage = 'C:/Users/pawit/Desktop/Dev-Ops/CEreform-frond-end/image';
 // C:\Users\pawit\Desktop\Dev-Ops\CEreform-frond-end\image
 // const urlImage = '';
